25d68b3ceda01a463cae470f1c44b009f25f1dc5
once-ler <htao@eztier.com>
2018-05-13 16:54:45 -0400

    Add logging on HL7 parse error

--- src/main/scala/hl7mock/Hapi.scala
+++ src/main/scala/hl7mock/Hapi.scala
@@ -1,5 +1,8 @@
 package com.eztier.hl7mock
 
+import java.io.{PrintWriter, StringWriter}
+
+import akka.event.LoggingAdapter
 import ca.uhn.hl7v2.{DefaultHapiContext, HL7Exception}
 import ca.uhn.hl7v2.model.Message
 import ca.uhn.hl7v2.parser.{CanonicalModelClassFactory, EncodingNotSupportedException}
@@ -13,12 +16,22 @@ object Hapi {
     hapiContext.getPipeParser()
   }
 
-  def parseMessage(in: String): Option[Message] =
+  def parseMessage(in: String)(implicit logger: LoggingAdapter): Option[Message] =
     try {
       val a = pipeParser.parse(in)
       Some(a)
     } catch {
-      case e: EncodingNotSupportedException => { e.printStackTrace(); None }
-      case e1: HL7Exception => { e1.printStackTrace(); None }
+      case e: EncodingNotSupportedException => {
+        val sw = new StringWriter
+        e.printStackTrace(new PrintWriter(sw))
+        logger.error(sw.toString)
+        None
+      }
+      case e1: HL7Exception => {
+        val sw = new StringWriter
+        e1.printStackTrace(new PrintWriter(sw))
+        logger.error(sw.toString)
+        None
+      }
     }
 }
