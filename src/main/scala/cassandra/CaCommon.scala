819179105f234b0034f1a22f95a587430e468bdf
once-ler <htao@eztier.com>
2018-05-22 16:04:14 -0400

    Add getFieldValues helper function

--- src/main/scala/cassandra/CaCommon.scala
+++ src/main/scala/cassandra/CaCommon.scala
@@ -1,12 +1,24 @@
 package com.eztier.cassandra
 
 object CaCommon {
-  def getFieldNames(cc: AnyRef) =
-    (Seq[String]() /: cc.getClass.getDeclaredFields) {
+  def getFieldValues(in: AnyRef,
+                     filterFunc: java.lang.reflect.Field => Boolean = (_) => true,
+                     formatFunc: Any => Any = a => a) = {
+    (List[Any]() /: in.getClass.getDeclaredFields.filter(filterFunc)) {
       (a, f) =>
         f.setAccessible(true)
-        a :+ f.getName
+        a :+ formatFunc(f.get(in))
     }
+  }
+
+  def getFieldNames(in: AnyRef,
+                    filterFunc: java.lang.reflect.Field => Boolean = (_) => true,
+                    formatFunc: String => String = a => a) = {
+    (List[String]() /: in.getClass.getDeclaredFields.filter(filterFunc)) {
+      (a, f) =>
+        a :+ formatFunc(f.getName)
+    }
+  }
 
   def camelToUnderscores(name: String) = "[A-Z\\d]".r.replaceAllIn(name.charAt(0).toLower.toString + name.substring(1), {m =>
     "_" + m.group(0).toLowerCase()
@@ -15,4 +27,6 @@ object CaCommon {
   def underscoreToCamel(name: String) = "_([a-z\\d])".r.replaceAllIn(name, {m =>
     m.group(1).toUpperCase()
   })
+
+  def camelToSpaces(name: String) = "[A-Z\\d]".r.replaceAllIn(name, (m => " " + m.group(0)))
 }
