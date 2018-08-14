5a3054cf52b4844f6b9be0ff4aac6114320aac70
once-ler <htao@eztier.com>
2018-08-14 12:43:59 -0400

    Generate UDT or table create script based on class meta

--- src/main/scala/cassandra/CaUdt.scala
+++ src/main/scala/cassandra/CaUdt.scala
@@ -22,6 +22,7 @@ trait CaUdt {
   }
 
   def handleSeqString(l: Seq[String]): String = makeSeqString(l)
+  def handleSeqBigDecimal(l: Seq[BigDecimal]): String = makeSeqString(l, false)
   def handleSeqDouble(l: Seq[Double]): String = makeSeqString(l, false)
   def handleSeqFloat(l: Seq[Float]): String = makeSeqString(l, false)
   def handleSeqLong(l: Seq[Long]): String = makeSeqString(l, false)
@@ -50,16 +51,57 @@ trait CaUdt {
             case a: CaUdt => handleCaUdt(a)
             case a: String => "'" + a + "'"
             case a: Date => "'" + handleDate(a) + "'"
+            case a: java.math.BigDecimal => a.toString
             case a if pat.findFirstIn(f.getType.getSimpleName) != None => a.toString
             case a: Seq[CaUdt] => handleSeqCaUdt(o.asInstanceOf[Seq[CaUdt]])
             case a: Seq[String] => handleSeqString(o.asInstanceOf[Seq[String]])
             case a: Seq[Date] => handleSeqDate(o.asInstanceOf[Seq[Date]])
+            case a: Seq[BigDecimal] => handleSeqBigDecimal(o.asInstanceOf[Seq[BigDecimal]])
             case a: Seq[Double] => handleSeqDouble(o.asInstanceOf[Seq[Double]])
             case a: Seq[Float] => handleSeqFloat(o.asInstanceOf[Seq[Float]])
             case a: Seq[Long] => handleSeqLong(o.asInstanceOf[Seq[Long]])
             case a: Seq[Int] => handleSeqInt(o.asInstanceOf[Seq[Int]])
             case a: Seq[Short] => handleSeqShort(o.asInstanceOf[Seq[Short]])
             case a: Seq[Byte] => handleSeqByte(o.asInstanceOf[Seq[Byte]])
+            case _ => ""
+          }
+        }
+
+        a + (camelToUnderscores(f.getName) -> s)
+    }
+  }
+
+  def toCaType() = {
+    val cc = this
+    (Map[String, String]() /: cc.getClass.getDeclaredFields) {
+      (a, f) =>
+        f.setAccessible(true)
+        val o = f.get(cc)
+
+        val s: String = {
+          o match {
+            case a: CaUdt => "frozen<" + camelToUnderscores(f.getName) + ">"
+            case a: String => "text"
+            case a: Date => "timestamp"
+            case a: java.math.BigDecimal => "decimal"
+            case a: java.lang.Float => "float"
+            case a: java.lang.Double => "double"
+            case a: java.lang.Long => "bigint"
+            case a: java.lang.Integer => "int"
+            case a: java.lang.Short => "smallint"
+            case a: java.lang.Byte => "tinyint"
+            case a: java.lang.Boolean => "boolean"
+            case a: Seq[CaUdt] => "list<frozen<" + camelToUnderscores(f.getName) + ">>"
+            case a: Seq[String] => "list<text>"
+            case a: Seq[Date] => "list<timestamp>"
+            case a: Seq[BigDecimal] => "list<decimal>"
+            case a: Seq[Double] => "list<double>"
+            case a: Seq[Float] => "list<float>"
+            case a: Seq[Long] => "list<bigint>"
+            case a: Seq[Int] => "list<int>"
+            case a: Seq[Short] => "list<smallint>"
+            case a: Seq[Byte] => "list<tinyint>"
+            case a: Seq[Boolean] => "list<boolean>"
             case _ => ""
           }
         }
@@ -79,43 +121,19 @@ trait CaUdt {
     "{" + b.mkString(",") + "}"
   }
 
-  def getCreateStmt(keySpace: String, objectType: String = "type") = {
-    val cc = this
-    (Map[String, String]() /: cc.getClass.getDeclaredFields) {
-      (a, f) =>
-        f.setAccessible(true)
-        val o = f.get(cc)
+  def getCreateStmt(objectType: String = "type") = {
+    val m = toCaType()
 
-        val s: String = {
-          o match {
-            case a: CaUdt => "frozen<" + keySpace + "." + camelToUnderscores(f.getName) + ">"
-            case a: String => "text"
-            case a: Date => "timestamp"
-            case a: java.math.BigDecimal => "decimal"
-            case a: java.lang.Float => "float"
-            case a: java.lang.Double => "double"
-            case a: java.lang.Long => "bigint"
-            case a: java.lang.Integer => "int"
-            case a: java.lang.Short => "smallint"
-            case a: java.lang.Byte => "tinyint"
-            case a: java.lang.Boolean => "boolean"
-            case a: Seq[CaUdt] => "list<frozen<" + keySpace + "." + camelToUnderscores(f.getName) + ">>"
-            case a: Seq[String] => "list<text>"
-            case a: Seq[Date] => "list<timestamp>"
-            case a: Seq[BigDecimal] => "list<decimal>"
-            case a: Seq[Double] => "list<double>"
-            case a: Seq[Float] => "list<float>"
-            case a: Seq[Long] => "list<bigint>"
-            case a: Seq[Int] => "list<int>"
-            case a: Seq[Short] => "list<smallint>"
-            case a: Seq[Byte] => "list<tinyint>"
-            case a: Seq[Boolean] => "list<boolean>"
-            case _ => ""
-          }
-        }
-
-        a + (camelToUnderscores(f.getName) -> s)
+    val f = (ArrayBuffer[String]() /: m) {
+      (a, n) =>
+      a += n._1 + " " + n._2
+      a
     }
+
+    s"""create ${objectType} if not exists ${camelToUnderscores(getClass().getSimpleName)} (
+       |${f.mkString(",")}
+       |);
+     """.stripLineEnd.stripMargin
   }
 }
 
@@ -131,5 +149,35 @@ trait CaTbl extends CaUdt {
     }
 
     s"""insert into ${camelToUnderscores(getClass().getSimpleName)} (${f._1.mkString(",")}) values (${f._2.mkString(",")})""".stripMargin
+  }
+
+  def getCreateStmt(partitionKeys: String*) = (clusteringKeys: String*) => (orderBy: Option[String], direction: Option[Int]) => {
+
+    val f = getClass.getDeclaredFields.map(_.getName)
+
+    val pk = partitionKeys.collect{
+      case a if f.find(_ == a) != None => camelToUnderscores(a)
+    }.mkString(",")
+
+    val ck = clusteringKeys.collect{
+      case a if f.find(_ == a) != None => camelToUnderscores(a)
+    }.mkString(",")
+
+    val sb = orderBy match {
+      case Some(a) if a.length > 0 && f.find(_ == a) != None =>
+        camelToUnderscores(a) + (
+          direction match {
+          case Some(b) => if (b > 0) "asc" else "desc"
+          case None => "asc"
+        })
+      case None => ""
+    }
+
+    val t = super.getCreateStmt("table")
+
+    t.substring(0, t.length - 2) +
+      s"((${pk})" +
+      (if (ck.length > 0) s",${ck})" else ")") +
+      sb
   }
 }
